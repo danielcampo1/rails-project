808299cae6b4b0669daa9a246a68dfb94c942e01
github username <github email address>
2021-01-14 19:55:51 -0600

    route for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   resources :travel_destinations
   resources :users
+
+  #home page
+  get "/" to: users#home
+
+
+
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
