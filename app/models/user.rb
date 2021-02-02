72a9304bc241371e198310503da80f32372f3b77
github username <github email address>
2021-02-02 16:59:51 -0600

    updates to validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
     has_many :countries, through: :destinations
 
     validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, unless: -> { :provider }
 
     def self.github_omniauth(auth)
         self.find_or_create_by(username: auth[:info][:nickname]) do |user|
             user.username = auth[:info][:nickname]
-            user.password = SecureRandom.hex(20)
+            # user.password = SecureRandom.hex(20)
         end
     end
 
