24541d4af67ab5305f0b631106338e17aa78b044
github username <github email address>
2021-02-03 17:23:13 -0600

    added simple search

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,7 +2,16 @@ class CountriesController < ApplicationController
     before_action :redirect_if_not_logged_in
     
     def index
-    @country = Country.all.order_country
+        if params[:country]
+            @country = Country.where('name LIKE ?', "%#{params[:country]}%").order_country
+          else
+            @country = Country.all.order_country
+          end
     end
+end
 
+private 
+
+def country_params
+    params.require(:country).permit(:description, :culture, :climate, :search, :name)
 end
